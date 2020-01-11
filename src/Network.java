633ce3b0c627596248fcc8a9a03e0a934f7ef70e
frooia <frooia77@gmail.com>
2020-01-10 20:26:21 -0500

    HITS finished, closeness failed, static SIR in progress

--- src/Network.java
+++ src/Network.java
@@ -1,8 +1,20 @@
 import edu.uci.ics.jung.algorithms.scoring.*;
+import edu.uci.ics.jung.algorithms.shortestpath.BFSDistanceLabeler;
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraDistance;
+import edu.uci.ics.jung.algorithms.shortestpath.Distance;
+import edu.uci.ics.jung.algorithms.shortestpath.UnweightedShortestPath;
 import edu.uci.ics.jung.graph.*;
+import edu.uci.ics.jung.graph.util.EdgeType;
 
+import java.io.*;
 import java.lang.reflect.Array;
+import java.nio.charset.Charset;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
+
+import static java.lang.Double.NaN;
 
 public class Network<V,E> extends SparseMultigraph<V,E> {
     private static final long serialVersionUID = 1L;
@@ -11,17 +23,13 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
     protected double totalBC; // total BetweennessCentrality
     protected V largestDegree;
     protected ArrayList<V> nodes;
-    protected HashMap<V, Double> bcentrality;
+    protected HashMap<Integer, Double> bcentrality;
 
     public Network(String n) {
         super();
         density = 0.0;
         totalBC = 0.0;
         name = n;
-    }
-
-    public void setBCentrality(HashMap<V, Double> bc) {
-        bcentrality = bc;
     }
 
     public void computeDensity() {
@@ -48,10 +56,6 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public void setTotalBC(double bc) {
         totalBC = bc;
     }
@@ -64,6 +68,10 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
         if (this.getVertexCount() != 0)
             return totalBC / this.getVertexCount();
         else return 0.0;
+    }
+
+    public HashMap<Integer, Double> getBcentrality() {
+        return bcentrality;
     }
 
     public V getLargestDegree() {
@@ -85,13 +93,150 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
         return (double)sumDegree/this.getVertexCount();
     }
 
-    public double getAverageCloseness() {
+    public double getAverageCloseness(V node) {
         ClosenessCentrality close = new ClosenessCentrality((Hypergraph) this);
         double sumCloseness = 0;
         for (V vertex : this.getVertices()) {
             sumCloseness+=close.getVertexScore(vertex);
         }
         return (double)sumCloseness/this.getVertexCount();
+    }
+
+    /*
+    public Set<V> getPredecessors(V vertex)
+    {
+        if (!containsVertex(vertex))
+            return null;
+
+        Set<V> preds = new HashSet<>();
+        for (E edge : getIncoming_internal(vertex)) {
+            if(getEdgeType(edge) == EdgeType.DIRECTED) {
+                preds.add(this.getSource(edge));
+            } else {
+                preds.add(getOpposite(vertex, edge));
+            }
+        }
+        return preds;
+    }
+
+
+    public Set<V> getAllPredecessors(V vertex)
+    {
+        Set<V> directPreds = getPredecessors(vertex);
+        Set<V> preds = new HashSet<>(directPreds);
+        if (preds.size() == 0) {
+            return Collections.emptySet();
+        }
+        else {
+            System.out.println();
+            for (V dp : directPreds) {
+                if (!preds.contains(dp)) {
+                    System.out.println(dp);
+                    preds.addAll(getAllPredecessors(dp));
+                }
+            }
+            return preds;
+        }
+    }
+
+    public boolean isStar(V node) {
+        V[] neighbors = (V[]) this.getNeighbors(node).toArray();
+        ArrayList<V> community = new ArrayList<>();
+        for (V n : neighbors) { community.add(n); }
+        for (V member : community) {
+            for (V neighbor : this.getNeighbors(member)) {
+                if (!community.contains(neighbor)) {
+                    return false;
+                }
+            }
+        }
+        System.out.print(node+" is a star with neighbors ");
+        for (V member : community) { System.out.print(member+" "); };
+        System.out.println();
+        return true;
+    }
+
+    public boolean isPair(V node) {
+        V[] community = (V[]) this.getNeighbors(node).toArray();
+        if (community.length == 1 && this.getNeighbors(community[0]).size() == 1) {
+            System.out.println(node+" is a pair with "+community[0]);
+            return true;
+        }
+        return false;
+    }
+
+    public double closeness(V node, boolean isIn) {
+        ArrayList<V> editedNodes = (ArrayList<V>) nodes.clone();
+        editedNodes.remove(node);
+        UnweightedShortestPath path = new UnweightedShortestPath((Hypergraph) this);
+        Map<V, Integer> distanceMap = new HashMap<>(path.getDistanceMap(node));
+        distanceMap.remove(node);
+        double value = 0.0;
+//        BFSDistanceLabeler<V, Integer> bfsdl = new BFSDistanceLabeler<>();
+//        bfsdl.labelDistances((Hypergraph<V, Integer>) this, new HashSet<>(editedNodes));
+        Set predecessors = this.getAllPredecessors(node);
+        if (isIn) {
+            System.out.println("Predecessors: "+predecessors);
+            for (Object element : predecessors) {
+                    value += (int) path.getDistance(element, node);
+            }
+        }
+        else {
+            System.out.println("Successors: "+distanceMap);
+            for (Map.Entry<V, Integer> element : distanceMap.entrySet()) {
+                if (((Hypergraph) this).getEdgeType(((Hypergraph) this).findEdge(node, element.getKey())).compareTo(EdgeType.DIRECTED) == 0) {
+                    value += element.getValue();
+                }
+            }
+        }
+        return value / (nodes.size() - 1);
+    }
+
+    public void setCloseness() throws IOException {
+        File readFrom = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/data_"+name+".tsv");
+        File writeTo = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/closeness_"+name+".tsv");
+        PrintWriter dos = new PrintWriter(writeTo);
+        Scanner inFile = new Scanner(readFrom);
+        dos.println(inFile.nextLine()+"\tIn-Closeness\tOut-Closeness");
+        HashMap<V, Double> inMap = new HashMap<V, Double>();
+        HashMap<V, Double> outMap = new HashMap<V, Double>();
+        int count = 0;
+        double inclose;
+        double outclose;
+        for (V node : nodes) {
+            count++;
+            inclose = this.closeness(node, true);
+            outclose = this.closeness(node, false);
+            inMap.put(node, inclose);
+            outMap.put(node, outclose);
+            System.out.print(count+". "+node+": In-Closeness = "+inclose+" Out-Closeness = "+outclose);
+            System.out.print(" and has "+((Hypergraph) this).getPredecessors(node).size()+" predecessors and "+((Hypergraph) this).getSuccessors(node).size()+" successors: ");
+//            for (V n : (V[])((Hypergraph) this).getPredecessors(node).toArray()) { System.out.print(n+" ("+path.getDistance(node, n)+" "+((Hypergraph) this).getEdgeType(((Hypergraph) this).findEdge(node, n))+")  ");}
+            System.out.println();
+
+            String fileLine = inFile.nextLine();
+            ArrayList<String> line = new ArrayList();
+            boolean x = Collections.addAll(line, fileLine.split("\t"));
+            line.add(Double.toString(inclose));
+            line.add(Double.toString(outclose));
+            String l = "";
+            for (String s : line) { l+=s+"\t"; }
+            dos.println(l);
+        }
+    }
+*/
+
+    public Map<V, Double> calcHits(boolean isHub) {
+        HITS<V, E> ranker = new HITS<>((Graph<V, E>) this);
+        ranker.evaluate();
+        Map<V, Double> scoreMap = new HashMap<>();
+        for (V node : nodes) {
+            HITS.Scores hitScores = ranker.getVertexScore(node);
+//            scoreMap.put(node, hitScores.hub);
+            if (isHub) { scoreMap.put(node, hitScores.hub); }
+            else { scoreMap.put(node, hitScores.authority); }
+        }
+        return scoreMap;
     }
 
     //runs SIR model with infection rate beta
@@ -102,13 +247,12 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
         infected.add(starter);
         int count = 0;
         while (infected.size() > 0) {
-            for (int i = 0; i < infected.size(); i++) { // for every infected node
+            for (int i = 0; i < infected.size(); i++) {
                 V node = infected.get(i);
                 int n = 0;
                 V[] neighbors = (V[]) this.getNeighbors(node).toArray();
                 for (int j = 0; j < neighbors.length; j++) {
-                    if (!recovered.contains(neighbors[j]) && random.nextDouble() < beta) {
-                        // add the neighbor to infected with probability beta
+                    if (!infected.contains(neighbors[j]) && !recovered.contains(neighbors[j]) && random.nextDouble() < beta) {
                         infected.add(neighbors[j]);
                     }
                     n++;
@@ -124,5 +268,120 @@ public class Network<V,E> extends SparseMultigraph<V,E> {
         }
         //System.out.println("Count of recovered nodes: "+count);
         return recovered.size();
+    }
+
+    public Map<V, Double> runSIR(double beta) {
+        Map<V, Double> recoveredMap = new HashMap<>();
+        for (V node : nodes) {
+            recoveredMap.put(node, sir(node, beta) / (double) nodes.size());
+        }
+        return recoveredMap;
+    }
+
+    public void readBC(String p, String name) {
+        HashMap<Integer, Double> map = new HashMap<Integer, Double>();
+        // Define charset and file path
+        Charset c = Charset.forName("US-ASCII");
+        // assumes the file is located in the same folder as the java class
+        Path f = FileSystems.getDefault().getPath(p, name);
+
+        // open a file using the specified charset
+        try (BufferedReader r = Files.newBufferedReader(f, c)) {
+            String ln = null; // a line in the file
+            boolean start = false; // a flag to indicate start of reading data
+
+            int numOriginalPos = 0;
+            int numNodes = 0;
+            // for each line, process the string to extract needed data
+            while ((ln = r.readLine()) != null) {
+                // split the line by comma and form an array of the component strings
+                String[] str = ln.split(",");
+                int j = 0;
+                // print out each component in one line
+                for (int i = 0; i < str.length; i++) {
+                    if (!start && str[i].contains("edu.uci.ics.jung.algorithms.importance.Ranking")) start = true;
+                    if (start && numNodes < this.getVertexCount()) {
+                        // Your code starts here: capture needed data and store them in a hashmap
+                        //extra condition for if statement: && net.getNodes().contains(Integer.parseInt(str[i].substring(14)))
+                        if (str[i].contains("originalPos")) {
+                            numOriginalPos++;
+                            int node = Integer.parseInt(str[i+3].substring(8, str[i+3].length()-2));
+                            if (this.getNodes().contains(node) && !map.containsKey(node)) {
+                                numNodes++;
+                                map.put(node, Double.parseDouble(str[i+1].substring(12)));
+//                                System.out.println(numNodes+". "+node+" "+Double.parseDouble(str[i+1].substring(12)));
+                            }
+                        }
+                        // Your code ends here
+                    }
+                }
+            }
+            System.out.println("numOriginalPos: " + numOriginalPos);
+            System.out.println("numNodes: " + numNodes+" should be "+this.getNodes().size());
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
+        System.out.println("Map size: "+map.size());
+        bcentrality = map;
+    }
+
+    public void writeData() throws IOException {
+        File file = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/data_"+name+".tsv");
+        FileWriter fos = new FileWriter(file);
+        PrintWriter dos = new PrintWriter(fos);
+        dos.write("Node\tIn-Degree\tOut-Degree\tBCentrality\t\n");
+        for (V node : nodes) {
+            dos.print(node+"\t"+this.getPredecessors(node).size()+"\t"+this.getSuccessors(node).size()+"\t"+bcentrality.get(node)+"\t\n");
+        }
+        dos.close();
+        fos.close();
+        System.out.println("written");
+    }
+
+    // adds a node property to the end of each line, effectively makes a new column
+    public void editData(String title, Map data) throws IOException {
+        File readFrom = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/data_"+name+".tsv");
+        File writeTo = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/editdata_"+name+".tsv");
+        Scanner inFile = new Scanner(readFrom);
+        ArrayList<String> lines = new ArrayList<>();
+        while (inFile.hasNext()) {
+            String fileLine = inFile.nextLine();
+            ArrayList<String> line = new ArrayList();
+            boolean x = Collections.addAll(line, fileLine.split("\t"));
+            if (line.get(0).equals("Node")) { line.add(title); }
+            else { line.add(data.get(Integer.parseInt(line.get(0))).toString()); }
+            String l = "";
+            for (String s : line) {
+                l+= s+"\t";
+            }
+            lines.add(l);
+        }
+        Files.write(writeTo.toPath(), lines, Charset.defaultCharset());
+    }
+
+    // insert your own file path and name
+    public void editData(String title, Map data, String writeToPath) throws IOException {
+        File readFrom = new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+name+"/data_"+name+".tsv");
+        File writeTo = new File(writeToPath);
+        Scanner inFile = new Scanner(readFrom);
+        ArrayList<String> lines = new ArrayList<>();
+        while (inFile.hasNext()) {
+            String fileLine = inFile.nextLine();
+            ArrayList<String> line = new ArrayList();
+            boolean x = Collections.addAll(line, fileLine.split("\t"));
+            if (line.get(0).equals("Node")) { line.add(title); }
+            else { line.add(data.get(Integer.parseInt(line.get(0))).toString()); }
+            String l = "";
+            for (String s : line) {
+                l+= s+"\t";
+            }
+            lines.add(l);
+        }
+        Files.write(writeTo.toPath(), lines, Charset.defaultCharset());
+    }
+
+    public String toString() {
+        // name n m density avgdegree largestdegreenode
+        return name+"\t"+this.getVertexCount()+"\t"+this.getEdgeCount()+"\t"+this.getDensity()+"\t"+this.getAverageDegree()+"\t"+this.getLargestDegree()+"\t"+totalBC;
     }
 }
--- src/NetworkHandler.java
+++ src/NetworkHandler.java
@@ -43,7 +43,7 @@ public class NetworkHandler {
                 try {
 
                     //String a = new File(".").getAbsolutePath() +"/";
-                    String p = "input/" + args[i] + "/";
+                    String p = "/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/" + args[i] + "/";
                     String f = "network_"+args[i]+".dat";
                     System.out.println("File: "+p+f);
                     net = this.readNet(p + f);
@@ -51,7 +51,33 @@ public class NetworkHandler {
                     System.out.println("EdgeCount = "+net.getEdgeCount());
                     net.setNodes();
                     System.out.println("NodeCount = "+net.getVertexCount());
-                    System.out.println();
+
+                    /*
+                    int in0 = 0; int in1 = 0; int in2 = 0; int in310 = 0; int in100 = 0; int in1000 = 0; int in1000p = 0;
+                    for (int node : net.getNodes()) {
+                        if (net.getPredecessors(node).size() == 0) { in0++; }
+                        else if (net.getPredecessors(node).size() == 1) { in1++; }
+                        else if (net.getPredecessors(node).size() == 2) { in2++; }
+                        else if (net.getPredecessors(node).size() <= 10) { in310++; }
+                        else if (net.getPredecessors(node).size() <= 100) { in100++; }
+                        else if (net.getPredecessors(node).size() <= 1000) { in1000++; }
+                        else { in1000p++; }
+                    }
+                    System.out.println("In-degrees:\n0: "+in0+"\n1: "+in1+"\n2: "+in2+"\n3-10: "+in310+"\n11-100: "+in100+"\n101-1000: "+in1000+"\n1001 and more: "+in1000p);
+
+                    int out0 = 0; int out1 = 0; int out2 = 0; int out310 = 0; int out100 = 0; int out1000 = 0; int out1000p = 0;
+                    for (int node : net.getNodes()) {
+                        if (net.getSuccessors(node).size() == 0) { out0++; }
+                        else if (net.getSuccessors(node).size() == 1) { out1++; }
+                        else if (net.getSuccessors(node).size() == 2) { in2++; }
+                        else if (net.getSuccessors(node).size() <= 10) { out310++; }
+                        else if (net.getSuccessors(node).size() <= 100) { out100++; }
+                        else if (net.getSuccessors(node).size() <= 1000) { out1000++; }
+                        else { out1000p++; }
+                    }
+                    System.out.println("Out-degrees:\n0: "+out0+"\n1: "+out1+"\n2: "+out2+"\n3-10: "+out310+"\n11-100: "+out100+"\n101-1000: "+out1000+"\n1001 and more: "+out1000p);
+                     */
+
                     netList.add(net);
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -155,26 +181,45 @@ public class NetworkHandler {
         return sameNodes;
     }
 
-
-
-    public static void main(String[] args) {
-        //Usage: NetworkHandler <Path to DB config file> <Path to store network files> <[Optional: DEBUG]>
-        NetworkHandler nh = new NetworkHandler("./conf/conf_db.txt", args[0]);
-        String [ ] dirs = (new File("input")).list();
-        ArrayList<Network<Integer, Integer>> netList = nh.init(dirs);
-        for (int i = 0; i < netList.size(); i++) {
-            //netList.get(i).setBCentrality(nh.readBC("input/"+netList.get(i).getName(), "BCentrality_"+netList.get(i).getName()+".dat", netList.get(i)));
-            int node = netList.get(i).getLargestDegree();
-            System.out.println("Largest degree node: " + node);
-            System.out.println("Recovered nodes: "+netList.get(i).sir(node, 0.2));
+    public static int sirTemp(ArrayList<Network<Integer, Integer>> networks, int starter, double beta) {
+        Random random = new Random();
+        ArrayList<Integer> infected = new ArrayList();
+        ArrayList<Integer> recovered = new ArrayList();
+        infected.add(starter);
+        int t = 0;
+        while (t < networks.size() && infected.size() > 0) {
+            for (int i = 0; i < infected.size(); i++) {
+                int node = infected.get(t);
+                if (networks.get(t).containsVertex(node)) {
+                    ArrayList neighbors = new ArrayList();
+                    neighbors.addAll(networks.get(t).getNeighbors(node));
+                    for (int j = 0; j < neighbors.size(); j++) {
+                        if (!infected.contains(neighbors.get(j)) && !recovered.contains(neighbors.get(j)) && random.nextDouble() < beta) {
+                            infected.add((int)neighbors.get(j));
+                        }
+                    }
+                }
+                recovered.add(node);
+                infected.remove(node);
+            }
+            t++;
         }
-
-        //System.out.println("Average degree: "+network.getAverageDegree());
-        //ClosenessCentrality close = new ClosenessCentrality((Hypergraph) network);
-        //System.out.println("Closeness of largest degree node: "+close.getVertexScore(network.getLargestDegree()));;
-
-        //sameNodes=nh.getIntersection(sameNodes, network);
-        //System.out.println("Intersection: "+sameNodes.size()+" nodes");
+        return recovered.size();
     }
 
+    public static void main(String[] args) throws IOException {
+        //Usage: NetworkHandler <Path to DB config file> <Path to store network files> <[Optional: DEBUG]>
+        NetworkHandler nh = new NetworkHandler("./conf/conf_db.txt", args[0]);
+        String [ ] dirs = (new File("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research")).list();
+        Arrays.sort(dirs);
+        ArrayList<Network<Integer, Integer>> netList = nh.init(Arrays.copyOfRange(dirs, 1, 2));
+        for (Network net : netList) {
+//            net.readBC("/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+net.getName(), "BCentrality_"+net.getName()+".dat");
+//            net.writeData();
+//            net.editData("HubScore", net.calcHits(true), "/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+net.getName()+"/data_"+net.getName()+".tsv");
+//            net.editData("AuthorityScore", net.calcHits(false), "/Users/lydia/Documents/GitHub/backup_CyberTF/network_research/"+net.getName()+"/data_"+net.getName()+".tsv");
+            net.editData("SIR", net.runSIR(0.2));
+        }
+//        System.out.println(sirTemp(netList, netList.get(0).getLargestDegree(), 0.2));
+    }
 }
